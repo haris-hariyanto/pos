c1a7c9dc63483e75e9a46e2e013346f5778d80af
Haris Hariyanto <harishariyanto313@gmail.com>
2023-01-09 18:11:31 +0700

    Database bug DigitalOcean

--- database/migrations/2022_09_11_123344_create_groups_permissions_table.php
+++ database/migrations/2022_09_11_123344_create_groups_permissions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('groups_permissions', function (Blueprint $table) {
+            $table->id();
             $table->integer('group_id');
             $table->string('permission');
             $table->string('type');
