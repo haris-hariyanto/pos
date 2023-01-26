916f36316feabef281d9fe6ec32a0f84792d1bca
Haris Hariyanto <harishariyanto313@gmail.com>
2023-01-26 07:36:04 +0700

    x

---
+++ app/Console/Commands/Content/ChangeCountry.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands\Content;
+
+use Illuminate\Console\Command;
+use App\Models\Location\Place;
+use App\Models\Location\Country;
+
+class ChangeCountry extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'content:change-country';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $countryName = $this->ask('Nama negara');
+        $places = Place::where('address', 'like', '%' . $countryName)->get();
+
+        if (count($places) > 0) {
+            $changeToCountry = $this->ask('Negara baru');
+            $changeToContinent = $this->ask('Benuar baru');
+
+            $country = Country::where('name', $changeToCountry)->where('continent', $changeToContinent)->first();
+
+            if ($country) {
+                foreach ($places as $place) {
+                    $this->line('[ * ] Mengubah negara : ' . $place->name);
+                    $place->update([
+                        'country' => $country->name,
+                        'continent' => $continent->name,
+                    ]);
+                }
+            }
+            else {
+                $this->info('[ * ] Negara tidak ditemukan');    
+            }
+        }
+        else {
+            $this->info('[ * ] Tidak ada tempat yang ditemukan');
+        }
+    }
+}
