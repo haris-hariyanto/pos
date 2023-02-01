67048032dfa2934f8b428b5f8dfec8ecec74936b
Haris Hariyanto <harishariyanto313@gmail.com>
2023-02-01 18:24:08 +0700

    x

--- routes/web.php
+++ routes/web.php
@@ -16,31 +16,60 @@ use App\Http\Controllers;
 
 Route::get('/', [Controllers\Main\HomeController::class, 'index'])->name('index');
 
-Route::get('/continent/{continent}', [Controllers\Main\Content\ContinentController::class, 'index'])->name('continent');
-Route::get('/continent', function () {
-    return redirect()->route('index');
-});
-
-Route::get('/country/{country}', [Controllers\Main\Content\CountryController::class, 'index'])->name('country');
-Route::get('/country/{country}/cities', [Controllers\Main\Content\CountryController::class, 'cities'])->name('country.cities');
-Route::get('/country/{country}/states', [Controllers\Main\Content\CountryController::class, 'states'])->name('country.states');
-Route::get('/country/{country}/places/{place}', [Controllers\Main\Content\CountryController::class, 'places'])->name('country.places');
-Route::get('/country', function () {
-    return redirect()->route('index');
-});
-
-Route::get('/place/{place}', [Controllers\Main\Content\PlaceController::class, 'index'])->name('place');
-Route::get('/place', function () {
-    return redirect()->route('index');
-});
-
-Route::get('/hotel/{hotel}', [Controllers\Main\Content\HotelController::class, 'index'])->name('hotel');
-Route::get('/hotel/{type}/{location}', [Controllers\Main\Content\LocationController::class, 'index'])->name('hotel.location');
-Route::get('/hotel', function () {
-    return redirect()->route('index');
-});
-
-Route::get('/search', [Controllers\Main\Content\SearchController::class, 'index'])->name('search');
+if (config('app.locale') == 'id') {
+    Route::get('/benua/{continent}', [Controllers\Main\Content\ContinentController::class, 'index'])->name('continent');
+    Route::get('/benua', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/negara/{country}', [Controllers\Main\Content\CountryController::class, 'index'])->name('country');
+    Route::get('/negara/{country}/kota', [Controllers\Main\Content\CountryController::class, 'cities'])->name('country.cities');
+    Route::get('/negara/{country}/state', [Controllers\Main\Content\CountryController::class, 'states'])->name('country.states');
+    Route::get('/negara/{country}/tempat/{place}', [Controllers\Main\Content\CountryController::class, 'places'])->name('country.places');
+    Route::get('/negara', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/tempat/{place}', [Controllers\Main\Content\PlaceController::class, 'index'])->name('place');
+    Route::get('/tempat', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/hotel/{hotel}', [Controllers\Main\Content\HotelController::class, 'index'])->name('hotel');
+    Route::get('/hotel/{type}/{location}', [Controllers\Main\Content\LocationController::class, 'index'])->name('hotel.location');
+    Route::get('/hotel', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/search', [Controllers\Main\Content\SearchController::class, 'index'])->name('search');
+}
+else {
+    Route::get('/continent/{continent}', [Controllers\Main\Content\ContinentController::class, 'index'])->name('continent');
+    Route::get('/continent', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/country/{country}', [Controllers\Main\Content\CountryController::class, 'index'])->name('country');
+    Route::get('/country/{country}/cities', [Controllers\Main\Content\CountryController::class, 'cities'])->name('country.cities');
+    Route::get('/country/{country}/states', [Controllers\Main\Content\CountryController::class, 'states'])->name('country.states');
+    Route::get('/country/{country}/places/{place}', [Controllers\Main\Content\CountryController::class, 'places'])->name('country.places');
+    Route::get('/country', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/place/{place}', [Controllers\Main\Content\PlaceController::class, 'index'])->name('place');
+    Route::get('/place', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/hotel/{hotel}', [Controllers\Main\Content\HotelController::class, 'index'])->name('hotel');
+    Route::get('/hotel/{type}/{location}', [Controllers\Main\Content\LocationController::class, 'index'])->name('hotel.location');
+    Route::get('/hotel', function () {
+        return redirect()->route('index');
+    });
+    
+    Route::get('/search', [Controllers\Main\Content\SearchController::class, 'index'])->name('search');
+}
 
 Route::get('/p/{page}', [Controllers\Main\Misc\PageController::class, 'page'])->name('page');
 Route::get('/contact', [Controllers\Main\Misc\ContactController::class, 'contact'])->name('contact');
