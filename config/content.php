6275fc8f29eb13b4e43eef7d359425f0061a54a2
Haris Hariyanto <harishariyanto313@gmail.com>
2023-02-26 13:44:16 +0700

    x

--- config/content.php
+++ config/content.php
@@ -2,8 +2,8 @@
 
 return [
     'default_cover' => 'https://cdn.pixabay.com/photo/2022/12/23/16/03/sunrise-7674594_960_720.jpg',
-    'location_term_city' => 'city',
-    'location_term_state' => 'state',
+    'location_term_city' => 'kota',
+    'location_term_state' => 'provinsi',
 
     'hotels_pagination_items_per_page' => 25,
     'lazy_load' => env('LAZY_LOAD', false),
